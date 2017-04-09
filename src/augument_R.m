bfdfe8886607141e81f37b1650d87887d466e513
zyyyyyyyyy <youngzh@umich.edu>
2017-04-08 23:57:13 -0400

    add weight

--- src/augument_R.m
+++ src/augument_R.m
@@ -1,6 +1,10 @@
 function [R,d] = augument_R(R,d,state,control,robot_id, delta_time)
 %Augument for the new line of R
 %Return original R , augument_R, original d
+
+global INFO;                            % experiment configuration, should not be updated
+global PARAM;                           % global variables, should be updated
+
 [s_r,s_c] = size(state);
 last_state = state(:,s_c);
 augument_R =[];
@@ -17,7 +21,8 @@ for id = 1:4
 end
 
 augument_R = zeros(12,12);
-
+augument_I = zeros(12,12);
+M = diag([INFO.Sigma_v,INFO.Sigma_omega].^2);
 for i = 1:4
     id = robot_id(i);
     theta = last_state(3*id);
@@ -29,16 +34,29 @@ for i = 1:4
         Gt = [ 1, 0, (v*cos(theta + dt*omega))/omega - (v*cos(theta))/omega;
             0, 1, (v*sin(theta + dt*omega))/omega - (v*sin(theta))/omega;
             0, 0,                                                      1];
+        Vt = [   sin(theta + dt*omega)/omega - sin(theta)/omega, (v*sin(theta))/omega^2 - (v*sin(theta + dt*omega))/omega^2 + (dt*v*cos(theta + dt*omega))/omega;
+            cos(theta)/omega - cos(theta + dt*omega)/omega, (v*cos(theta + dt*omega))/omega^2 - (v*cos(theta))/omega^2 + (dt*v*sin(theta + dt*omega))/omega;
+            0,                                                                                              dt];
+        
     else
         Gt = [ 1, 0, -v*sin(theta);
-               0, 1,  v*cos(theta);
-               0, 0,             1];
+            0, 1,  v*cos(theta);
+            0, 0,             1];
+        Vt =[ cos(theta), 0;
+            sin(theta), 0;
+            0, 0];
     end
-    augument_R(3*i-2:3*i,3*i-2:3*i) = Gt;
+    
+    
+    Q =  Vt*M*Vt';
+    
+    w = inv((Q')^0.5);
+    augument_R(3*i-2:3*i,3*i-2:3*i) = w*Gt;
+    augument_I(3*i-2:3*i,3*i-2:3*i) = -w*eye(3);
 end
 
 [R_r,R_c] = size(R);
-augument_R = [zeros(12,R_c-12),augument_R,-1*eye(12)];
+augument_R = [zeros(12,R_c-12),augument_R,augument_I];
 lamda = zeros(12,1);
 
 [R, d] = Givens_Rotation(R, d, augument_R, lamda);
