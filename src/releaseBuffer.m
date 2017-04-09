0d9fc99a760ba732ecffcc8489927cea7d7ae42e
Uniericuni <uniericuni@gmail.com>
2017-04-09 17:26:10 -0400

    [Add] Scanmatching

--- src/releaseBuffer.m
+++ src/releaseBuffer.m
@@ -1,27 +1,28 @@
-function releaseBuffer(map_obs)
+function releaseBuffer()
 
 % global initiate
 global PARAM;
 global INFO;
 
 % scanmatching
+%{
 for i=1:PARAM.buff_size
     
     % if the local map overlapped
-    l_map = map_obs.map;
+    l_map = PARAM.local_buff.map;
     g_map = PARAM.obs_buff{PARAM.buff_size}.map;
     mex = l_map .* g_map;
     if sum(sum(mex(mex~=0))) > 20
         delta_ = alignment(l_map, g_map);
         % augment(  delta_, ...
-        %           map_obs.pose_col, ...
-        %           map_obs.robot_id, ...
+        %           PARAM.local_buff.pose_col, ...
+        %           PARAM.local_buff.robot_id, ...
         %           PARAM.obs_buff{PARAM.buff_size}.pose_col, ...
         %           PARAM.obs_buff{PARAM.buff_size}.robot_id );
     end
     
 end
-
+%}
 % merge global map
 
 
@@ -31,4 +32,8 @@ if PARAM.buff_size > INFO.GLOBAL_BUFF_SIZE
     PARAM.obs_buff{1:end-1} = PARAM.obs_buff{2:end};
     PARAM.buff_size = PARAM.buff_size - 1;
 end
-PARAM.obs_buff{PARAM.buff_size} = map_obs;
+PARAM.obs_buff{PARAM.buff_size} = PARAM.local_buff;
+PARAM.local_buff.map = PARAM.map(:,:);              % create local buffer
+PARAM.local_buff.pose_col = 0;
+PARAM.local_buff.robot_id = 0;
+PARAM.buff_size = 0;
