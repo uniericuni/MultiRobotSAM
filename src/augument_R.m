041516de376bb707f16e3dc16af785e942db619a
zyyyyyyyyy <youngzh@umich.edu>
2017-04-09 14:30:42 -0400

    update control model

--- src/augument_R.m
+++ src/augument_R.m
@@ -26,26 +26,18 @@ M = diag([INFO.Sigma_v,INFO.Sigma_omega].^2);
 for i = 1:4
     id = robot_id(i);
     theta = last_state(3*id);
-    omega = overall_control(3,i);
-    v = sqrt(overall_control(1,i)^2 + overall_control(2,i)^2);
+    v = overall_control(1,i);
+    omega = overall_control(2,i);
     dt = overall_dt(i);
     
-    if (omega ~= 0)
-        Gt = [ 1, 0, (v*cos(theta + dt*omega))/omega - (v*cos(theta))/omega;
-            0, 1, (v*sin(theta + dt*omega))/omega - (v*sin(theta))/omega;
-            0, 0,                                                      1];
-        Vt = [   sin(theta + dt*omega)/omega - sin(theta)/omega, (v*sin(theta))/omega^2 - (v*sin(theta + dt*omega))/omega^2 + (dt*v*cos(theta + dt*omega))/omega;
-            cos(theta)/omega - cos(theta + dt*omega)/omega, (v*cos(theta + dt*omega))/omega^2 - (v*cos(theta))/omega^2 + (dt*v*sin(theta + dt*omega))/omega;
-            0,                                                                                              dt];
-        
-    else
-        Gt = [ 1, 0, -v*sin(theta);
-            0, 1,  v*cos(theta);
-            0, 0,             1];
-        Vt =[ cos(theta), 0;
-            sin(theta), 0;
-            0, 0];
-    end
+    
+    Gt = [ 1, 0, -v*sin(theta + dt*omega);
+        0, 1,  v*cos(theta + dt*omega);
+        0, 0,                        1];
+    Vt = [ cos(theta + dt*omega), -dt*v*sin(theta + dt*omega);
+        sin(theta + dt*omega),  dt*v*cos(theta + dt*omega);
+        0,                          dt];
+    
     
     
     Q =  Vt*M*Vt';
@@ -62,4 +54,5 @@ lamda = zeros(12,1);
 [R, d] = Givens_Rotation(R, d, augument_R, lamda);
 
 end
+
 
