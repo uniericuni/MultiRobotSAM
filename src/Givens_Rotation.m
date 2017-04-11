65260252aec4868c3e5aaa5da366df4378343a01
zyyyyyyyyy <youngzh@umich.edu>
2017-04-11 17:11:18 -0400

    faster

--- src/Givens_Rotation.m
+++ src/Givens_Rotation.m
@@ -27,12 +27,22 @@ end
 d = [d;lamda];  % (M+K) x 1
 %% Start rrotation
 % start to do Givens Rotation, for every entry in the newly added matrix
-for i = 1:size(R,2)             % columns
-    for j = M+K:-1:max(M+1,i+1) % rows
-        if R(j,i) ~= 0
-            R
-            beta = R(j,i);  % a_ik::need to become zero
-            alpha = R(i,i); % a_kk::the diagonal element 
+R = sparse(R);
+
+% this is control
+if size(R,1) == size(R,2)
+    for r = M+1:M+K % rows::constant time because K is constant every time
+        ind = find(R(r,:)); % ind contains the col number of nonzero values
+        if length(ind)==0
+            continue;
+        end
+        while ind(1) < r
+            c = ind(1); % the column number 
+            
+            % calculate Givens matrix
+            beta = R(r,c);  % a_ik::need to become zero
+            alpha = R(c,c); % a_kk::the diagonal element
+
             if abs(beta) > abs(alpha)
                 CosPhi = alpha/(beta*(sqrt(1+(alpha/beta)^2)));
                 SinPhi = 1/sqrt(1+(alpha/beta)^2);
@@ -41,27 +51,70 @@ for i = 1:size(R,2)             % columns
                 CosPhi = 1/sqrt(1+(alpha/beta)^2);
             end
             % fill in Givens
-            Givens = eye(M+K,M+K); % size (M+K) x (M+K)
-            Givens(i,i) = CosPhi;
-            Givens(j,j) = CosPhi;
-            Givens(i,j) = SinPhi;
-            Givens(j,i) = -SinPhi;
+            Givens = eye(M+K-c+1,M+K-c+1); % size (M+K-c+1) x (M+K-c+1)
+            Givens(1,1) = CosPhi;
+            Givens(r-c+1,r-c+1) = CosPhi;
+            Givens(1,r-c+1) = SinPhi;
+            Givens(r-c+1,1) = -SinPhi;
             % apply Givens Rotation to R
-            R = Givens * R; 
+            R(c:end,c:end) = Givens * R(c:end,c:end); 
             % apply Givens Rotation to d
-            d = Givens * d;
+            d(c:end) = Givens * d(c:end);
+            
+            % rounding numbers for R 
+            R = R .* 1e12;
+            R = round(R);
+            R = R ./ 1e12;
+            % get the new indices
+            ind = find(R(r,:)); 
+            if length(ind)==0
+                break;
+            end
         end
     end
+    
+% this is observation
+elseif size(R,1) ~= size(R,2)
+    for r = M+1:M+K % rows::constant time because K is constant every time
+        ind = find(R(r,:)); % ind contains the col number of nonzero values
+        while isempty( find(ind,1) ) == 0 % there is non-zero elements
+            c = ind(1); % the column number 
+            
+            % calculate Givens matrix
+            beta = R(r,c);  % a_ik::need to become zero
+            alpha = R(c,c); % a_kk::the diagonal element
+            if abs(beta) > abs(alpha)
+                CosPhi = alpha/(beta*(sqrt(1+(alpha/beta)^2)));
+                SinPhi = 1/sqrt(1+(alpha/beta)^2);
+            else
+                SinPhi = beta/(alpha*(sqrt(1+(alpha/beta)^2)));
+                CosPhi = 1/sqrt(1+(alpha/beta)^2);
+            end
+            % fill in Givens
+            Givens = eye(M+K-c+1,M+K-c+1); % size (M+K-c+1) x (M+K-c+1)
+            Givens(1,1) = CosPhi;
+            Givens(r-c+1,r-c+1) = CosPhi;
+            Givens(1,r-c+1) = SinPhi;
+            Givens(r-c+1,1) = -SinPhi;
+            % apply Givens Rotation to R
+            R(c:end,c:end) = Givens * R(c:end,c:end); 
+            % apply Givens Rotation to d
+            d(c:end) = Givens * d(c:end);
+            
+            % rounding numbers for R 
+            R = R .* 1e12;
+            R = round(R);
+            R = R ./ 1e12;
+            % get the new indices
+            ind = find(R(r,:)); 
+        end
+    end
+
 end
 
 if M == K_2             % this is observation
     R = R(1:M,1:M);     % get size M x M
     d = d(1:M);         % get size M x 1
 end
-
-% rounding numbers for R 
-R = R .* 1e10;
-R = round(R);
-R = R ./ 1e10;
 
 end
